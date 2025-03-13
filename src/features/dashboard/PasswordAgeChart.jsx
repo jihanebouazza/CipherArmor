61618610625cac530a238043a9790d7c6d610bac
jiiiiiiiiiiiiiiiiiiiiiiii <110284532+jiiiiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2025-03-13 23:04:43 +0000

    Enhance password age chart UI

--- src/features/dashboard/PasswordAgeChart.jsx
+++ src/features/dashboard/PasswordAgeChart.jsx
@@ -8,6 +8,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+import { useDarkMode } from "../../contexts/DarkModeContext";
 
 ChartJS.register(
   LineElement,
@@ -100,6 +101,7 @@ const passwordData = [
 ];
 
 function PasswordAgeChart() {
+  const { isDarkMode } = useDarkMode();
   const { passwordAgeDistribution, passwordCreationTimeline } =
     getPasswordAgeGroups(passwordData);
 
@@ -109,27 +111,17 @@ function PasswordAgeChart() {
       {
         label: "Current Password Age",
         data: Object.values(passwordAgeDistribution),
-        borderColor: "#4caf50",
         borderDash: [5, 5],
-        backgroundColor: "rgba(76, 175, 80, 0.2)",
+        borderColor: "#003ca4",
+        backgroundColor: "#003ca4",
         tension: 0.3,
-        pointRadius: 5,
-        pointHoverRadius: 7,
-        pointStyle: "rectRounded",
-        borderRadius: 4,
-        rotation: 0,
       },
       {
         label: "Password Creation History",
         data: Object.values(passwordCreationTimeline),
-        borderColor: "#ff9800",
-        backgroundColor: "rgba(255, 152, 0, 0.2)",
+        backgroundColor: "#306cd3",
+        borderColor: "#306cd3",
         tension: 0.3,
-        pointRadius: 5,
-        pointHoverRadius: 7,
-        pointStyle: "rectRounded",
-        borderRadius: 4,
-        rotation: 0,
       },
     ],
   };
@@ -142,32 +134,57 @@ function PasswordAgeChart() {
         position: "top",
         labels: {
           usePointStyle: true,
-          boxWidth: 16,
-          boxHeight: 16,
-          borderRadius: 4,
-          pointStyle: "rectRounded",
-          padding: 20,
+          boxWidth: 12,
+          boxHeight: 12,
+          color: isDarkMode ? "#dddfe2" : "#0b0f14",
           font: {
-            size: 14,
+            family: "Work Sans",
+            size: 12,
+          },
+          generateLabels: (chart) => {
+            const labels =
+              ChartJS.defaults.plugins.legend.labels.generateLabels(chart);
+            return labels.map((label) => ({
+              ...label,
+              pointStyle: "rect",
+              rotation: 0,
+            }));
           },
         },
       },
       tooltip: {
         mode: "index",
         intersect: false,
-        usePointStyle: true,
+        backgroundColor: isDarkMode
+          ? "rgba(36 ,39, 44, 0.8)"
+          : "rgba(228,238,254,0.9)",
+        titleColor: isDarkMode ? "#edeeee" : "#0b0f14",
+        bodyColor: isDarkMode ? "#dddfe2" : "#1a1c21",
+        titleFont: {
+          family: "Work Sans",
+          weight: "600",
+          size: 14,
+        },
+        bodyFont: {
+          family: "Work Sans",
+        },
         callbacks: {
-          labelColor: (context) => ({
-            borderColor: context.dataset.borderColor,
-            backgroundColor: context.dataset.backgroundColor,
-            borderWidth: 2,
-            borderRadius: 4,
-            borderDash: context.dataset.borderDash || [],
-            width: 16,
-            height: 16,
-            pointStyle: "rectRounded",
-            rotation: 0,
-          }),
+          title: (context) => context[0].label,
+          label: (context) => {
+            const datasetLabel = context.dataset.label;
+            const value = context.parsed.y;
+
+            if (value === 0) {
+              return datasetLabel === "Current Password Age"
+                ? " No Updated Passwords"
+                : " No Created Passwords";
+            }
+
+            if (datasetLabel === "Current Password Age") {
+              return ` ${value} Updated Password${value !== 1 ? "s" : ""}`;
+            }
+            return ` ${value} Created Password${value !== 1 ? "s" : ""}`;
+          },
         },
       },
     },
@@ -175,20 +192,47 @@ function PasswordAgeChart() {
       y: {
         beginAtZero: true,
         grid: {
-          color: "rgba(0,0,0,0.05)",
+          color: isDarkMode ? "rgba(61,63, 66, 0.4)" : "rgba(103,106,111, 0.2)",
         },
         title: {
           display: true,
           text: "Number of Passwords",
+          font: {
+            family: "Work Sans",
+            size: 12,
+            weight: 500,
+          },
+          color: isDarkMode ? "#c6c7ca" : "#24272c",
+        },
+
+        ticks: {
+          stepSize: 1,
+          precision: 0,
+          color: isDarkMode ? "#dddfe2" : "#0b0f14",
+          font: {
+            family: "Work Sans",
+            size: 12,
+          },
+        },
+        border: {
+          color: isDarkMode ? "#676a6f" : "#8d9094",
+          width: 1,
         },
       },
       x: {
         grid: {
           display: false,
         },
-        title: {
-          display: true,
-          text: "Age Categories",
+        ticks: {
+          color: isDarkMode ? "#dddfe2" : "#0b0f14",
+          font: {
+            family: "Work Sans",
+            size: 12,
+          },
+        },
+        border: {
+          color: isDarkMode ? "#676a6f" : "#8d9094",
+          width: 1,
         },
       },
     },
